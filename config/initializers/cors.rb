6374df128779ee14b1e2ea8b311fe2a36bfad958
Emerson <emerson@Informations-MacBook-Pro-2.local>
2026-05-20 15:18:43 -0700

    Expand CORS to cover all routes including /actors auth endpoints

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # In production, restrict to your actual frontend domain
     origins *ENV.fetch("FRONTEND_ORIGIN", "*").split(",")
 
-    resource "/api/*",
+    resource "/*",
       headers: :any,
       methods: %i[get post put patch delete options head],
       expose: [ "Authorization" ]
